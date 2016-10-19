017be9a8b272c374b6293dd15808a341cf473498
rongbohou <rongbohou@163.com>
2016-10-19 20:42:47 +0800

    add file

---
+++ build/src/CMakeFiles/robustmatching.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/robustmatching.dir/robustmatching.cpp.o"
+  "../../bin/robustmatching.pdb"
+  "../../bin/robustmatching"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/robustmatching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/README.txt
@@ -0,0 +1,14 @@
+
+File:
+	estimateF.cpp
+correspond to Recipe:
+Computing the Fundamental Matrix of an Image Pair
+Files:
+	matcher.h
+	robustmatching.cpp
+correspond to Recipe:
+Matching Images using Random Sample Consensus
+File:
+	estimateH.cpp
+correspond to Recipe:
+Computing a homography between two images
